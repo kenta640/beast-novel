9a3df0f52c6e13dc612ad7fedd6e3f367afb8c05
kenta640 <kenta0803d@gmail.com>
2022-04-13 10:55:33 +1200

    fix attempt for page glitch

--- UserDetails/[user_id].tsx
+++ UserDetails/[user_id].tsx
@@ -1,19 +1,19 @@
 import { useRouter } from 'next/router'
-import {Navbar} from '../../components/Navbar'
-import { useFetchUser, } from "../api/user.hooks";
+import {Navbar} from '../components/Navbar'
+import { useFetchUser, } from "../pages/api/user.hooks";
 const UserPage=()=> {
     const router = useRouter()
     const { user_id } = router.query
     //console.log(user_id)
     //console.log(useFetchUser(user_id))
-    const {data: data, isLoading }= useFetchUser(user_id)
+    const {data: userData, isLoading }= useFetchUser(user_id)
     if (isLoading) {
         return(<>Loading...</>)
     }
     return(<>
     <Navbar/>
      <p>User: {
-     JSON.stringify(data.data)
+     JSON.stringify(userData.data)
      
      }</p>
     </>)
