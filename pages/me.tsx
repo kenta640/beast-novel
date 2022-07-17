1dbb98b25e59a20f9d2a6d699c864634da96142b
kenta640 <kenta0803d@gmail.com>
2022-07-17 12:56:04 +1200

    a

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,12 +1,59 @@
-import { useSession } from "next-auth/react"
 import Layout from "../components/layout"
-
+import styles from "../components/header.module.css"
+import { signIn, signOut, useSession } from "next-auth/react"
 export default function MePage() {
-  const { data } = useSession()
-
+  const { data: session, status } = useSession()
+  const loading = status === "loading"
   return (
     <Layout>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
+          {!session && (
+            <>
+              <span className={styles.notSignedInText}>
+                You are not signed in
+              </span>
+              <a
+                href={`/api/auth/signin`}
+                className={styles.buttonPrimary}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signIn()
+                }}
+              >
+                Sign in
+              </a>
+            </>
+          )}
+          {session?.user && (
+            <>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className={styles.button}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signOut()
+                }}
+              >
+                Sign out
+              </a>
+            </>
+          )}
+        </p>
     </Layout>
   )
 }
