ed463ebdefe41d9844a950fe7c05eb12577481b6
kenta640 <kenta0803d@gmail.com>
2022-07-25 21:52:19 +1200

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ function Home() {
                     // Note - error messages will vary depending on browser
                   } 
 
-                }, [session])
+                }, [session?.user])
 
     return (
       <div>
