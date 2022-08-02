4910968746bc8c88857b4bac1cad96c920f01c35
kenta640 <kenta0803d@gmail.com>
2022-08-02 17:09:16 +1200

    a

--- pages/userDetails.tsx
+++ pages/userDetails.tsx
@@ -1,8 +1,8 @@
 import { useRouter } from 'next/router'
-import {Navbar} from '../../components/Navbar'
-import { useFetchUser, } from "../api/hooks/user.hooks"
-import {NovelByUser} from "../../components/novelsByUser"
-import {useGetNovelByUser} from "../../components/api/novelByUser.hooks"
+import {Navbar} from '../components/Navbar'
+import { useFetchUser, } from "./api/hooks/user.hooks"
+import {NovelByUser} from "../components/novelsByUser"
+import {useGetNovelByUser} from "../components/api/novelByUser.hooks"
 import { useSession } from "next-auth/react"
 const UserPage=()=> {
     const router = useRouter()
@@ -10,28 +10,29 @@ const UserPage=()=> {
     //console.log(user_id)
     //console.log(useFetchUser(user_id))
      // @ts-ignore to ignore the type checking errors on the next line in a TypeScript
+     /**
     const {data: userData, isLoading }= useFetchUser(session?.user)
     if (isLoading) {
         return(<>
         <Navbar/>
         <p>Loading...</p>
         </>)
-    }
-    const user = userData.data
+    } 
+    const user = userData.data*/
     return(<>
     <Navbar/>
      <p>User: {
-     JSON.stringify(user.name)
+     //JSON.stringify(user.name)
      
      
      }</p>
      <p>{
-     JSON.stringify(user.email)
+     //JSON.stringify(user.email)
      
      
      }</p>
      
-     <NovelByUser user_id = {user.id}/>
+     {/**<NovelByUser user_id = {user.id}/>*/}
     </>)
 }
 
