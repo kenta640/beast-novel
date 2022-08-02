d0f87bfb82e925011d24139318e14c6287ee8640
kenta640 <kenta0803d@gmail.com>
2022-08-02 16:56:14 +1200

    a

--- pages/UserDetails/[user_id].tsx
+++ pages/UserDetails/[user_id].tsx
@@ -3,12 +3,14 @@ import {Navbar} from '../../components/Navbar'
 import { useFetchUser, } from "../api/hooks/user.hooks"
 import {NovelByUser} from "../../components/novelsByUser"
 import {useGetNovelByUser} from "../../components/api/novelByUser.hooks"
+import { useSession } from "next-auth/react"
 const UserPage=()=> {
     const router = useRouter()
-    const { user_id } = router.query
+    const { data: session, status } = useSession()
     //console.log(user_id)
     //console.log(useFetchUser(user_id))
-    const {data: userData, isLoading }= useFetchUser(user_id)
+     // @ts-ignore to ignore the type checking errors on the next line in a TypeScript
+    const {data: userData, isLoading }= useFetchUser(session?.user)
     if (isLoading) {
         return(<>
         <Navbar/>
