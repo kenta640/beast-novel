0646d13e8f08da8c2d7ddaa70a4741ebbbbd31ea
kenta640 <kenta0803d@gmail.com>
2022-08-05 12:03:56 +1200

    a

--- pages/write.tsx
+++ pages/write.tsx
@@ -61,9 +61,8 @@ function Write() {
     // @ts-ignore to ignore the type checking errors on the next line in a TypeScript
     const {data: userData, isLoading}=useFetchUser(session?.user?.email)
     
-    if(userData){
-      console.log(userData)
-    }
+      console.log(userData?.data)
+
     
     
     const user = session?.user
