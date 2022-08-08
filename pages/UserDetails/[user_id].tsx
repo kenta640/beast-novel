7ecf218a1beea8a87840e2754e37700b2e4cdfcf
kenta640 <kenta0803d@gmail.com>
2022-08-08 19:58:37 +1200

    a

--- pages/UserDetails/[user_id].tsx
+++ pages/UserDetails/[user_id].tsx
@@ -1,33 +1,34 @@
 import { useRouter } from 'next/router'
 import {Navbar} from '../../components/Navbar'
-import { useFetchUser, } from "../api/hooks/user.hooks"
+import { useFetchUserById, } from "../api/hooks/user.hooks"
 import {NovelByUser} from "../../components/novelsByUser"
 import {useGetNovelByUser} from "../../components/api/novelByUser.hooks"
 import { useSession } from "next-auth/react"
-const UserPage=()=> {
+const UserDetails=()=> {
     const router = useRouter()
     const { data: session, status } = useSession()
-    //console.log(user_id)
-    //console.log(useFetchUser(user_id))
+    
+    // @ts-ignore to ignore the type checking errors on the next line in a TypeScript
+    console.log(useFetchUserById(user_id))
      // @ts-ignore to ignore the type checking errors on the next line in a TypeScript
-     /**
-    const {data: userData, isLoading }= useFetchUser(session?.user)
+    
+    const {data: userData, isLoading }= useFetchUserById(user_id)
     if (isLoading) {
         return(<>
         <Navbar/>
         <p>Loading...</p>
         </>)
     } 
-    const user = userData.data*/
+    const user = userData.data
     return(<>
     <Navbar/>
      <p>User: {
-     //JSON.stringify(user.name)
+     JSON.stringify(user.name)
      
      
      }</p>
      <p>{
-     //JSON.stringify(user.email)
+     JSON.stringify(user.email)
      
      
      }</p>
--- pages/api/hooks/user.hooks.ts
+++ pages/api/hooks/user.hooks.ts
@@ -13,6 +13,28 @@ export const useGetUsers = async () =>
   );
 
 // @ts-ignore to ignore the type checking errors on the next line in a TypeScript
+export const useFetchUserById = (id) =>
+  
+  useQuery(
+    ServerStateKeysEnum.User,
+    () => fetch(`https://fastify-server-app.herokuapp.com/user/id/${id}`,// async await??
+    {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache'
+    })
+    .then(res => {
+      const response = res.json()
+      console.log(response)
+      return response
+    }
+    )
+    .catch(e=>{
+      console.log(e)
+    }) //Simple fetch function
+  );
+
+// @ts-ignore to ignore the type checking errors on the next line in a TypeScript
 export const useFetchUser = (email) =>
   
   useQuery(
