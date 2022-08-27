c30a76b006cfdd62c05bae8bae14a7dd9f054606
kenta640 <kenta0803d@gmail.com>
2022-08-27 13:59:30 +1200

    a

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,8 +5,9 @@ function About() {
         
       <div>
        <Navbar/>
-        <p className='font-sans ...'>Welcome</p>
-        
+        <p className='font-sans ...'>
+          Beast novel is run by one person. I will help you write novels.</p>
+        <p></p>
       </div>);
 }
  export default About
