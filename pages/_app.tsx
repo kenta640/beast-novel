ff7635db39a4440928ac15a21fe9fc183b6fb7be
kenta640 <kenta0803d@gmail.com>
2023-02-21 19:55:15 +1300

    bug fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ const queryCache = new QueryClient({
 });
 function MyApp({ Component, pageProps}: AppProps) {
   return (
-  <SessionProvider session={pageProps.session} refetchInterval={1}>
+  <SessionProvider refetchInterval={1}>
     <QueryClientProvider client={queryCache}>
     <Component {...pageProps} />
     </QueryClientProvider>
